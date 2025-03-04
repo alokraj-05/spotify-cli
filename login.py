eca64c035a3bc173937a9a7b3474a584141aa668
alokraj-05 <alok200504@gmail.com>
2025-03-05 02:12:02 +0530

    Authentication login

---
+++ login.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from spotipy import SpotifyOAuth
+import spotipy
+import json
+
+
+load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URL = os.getenv('REDIRECT_URI')
+SCOPE = "user-read-playback-state user-modify-playback-state"
+TOKEN_PATH = ".spotify_token.json"
+
+
+def login():
+  auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URL,scope=SCOPE,cache_path=TOKEN_PATH)
+
+  try:
+    if os.path.exists(TOKEN_PATH):
+      with open(TOKEN_PATH,"r") as file:
+        token_info = json.load(file)
+    else:
+      token_info = auth_manager.get_access_token()
+    
+    if token_info is None or auth_manager.is_token_expired(token_info):
+      token_info= auth_manager.refresh_access_token(token_info["refresh_token"])
+
+    access_token = token_info["access_token"]
+    sp =spotipy.Spotify(auth_manager=auth_manager)
+    print("✅ Logged in successfully")
+    return access_token
+  except Exception as e:
+    print("Error retreving access token: ",str(e))
+    return None
