56f7a3d29cf64f7e64cd7abf70988eaac72871fd
alokraj-05 <alok200504@gmail.com>
2025-03-07 23:51:11 +0530

    base change

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def authenticate():
 @click.command()
 @click.argument("artist_name")
 def artist_name(artist_name):
-    artist.artist_name("kk")
+    artist.artist_name(artist_name)
+
 
 @click.command()
 @click.argument("song_name")
@@ -41,11 +42,22 @@ def play_playlist(link):
 def pause():
     playback.pause()
 
+
+@click.command(name="merge-playlist")
+@click.argument("playlist_name")
+@click.argument("playlist1_url")
+@click.argument("playlist2_url")
+def merge_playlist_command(playlist_name,playlist1_url,playlist2_url):
+    playback.merge_playlist(playlist_name,playlist1_url,playlist2_url)
+
+
+
 main.add_command(authenticate)
 main.add_command(artist_name)
 main.add_command(play)
 main.add_command(play_playlist)
 main.add_command(pause)
+main.add_command(merge_playlist_command)
 
 
 if __name__ == "__main__":
